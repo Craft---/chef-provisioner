faee0c9845bab98c8008fde7990bacd3d71dde36
502375908 <ian.henry@ge.com>
2015-03-23 09:01:59 -0400

    aws config attr added

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,11 @@
 default['provisioner']['chefdk']['version'] = 'latest'
 
-# aws objects
-default['provisioner']['databag_name'] = ''
-default['provisioner']['key_name'] = ''
+# AWS objects
+default['provisioner']['aws']['databag_name'] = ''
+default['provisioner']['aws']['key_name'] = ''
+default['provisioner']['aws']['key_id'] = ''
+default['provisioner']['aws']['access_key'] = ''
+default['provisioner']['aws']['region'] = 'us-east-1'
 
 # general node options
 default['provisioner']['home'] = ENV['HOME']
@@ -10,7 +13,7 @@ default['provisioner']['user'] = 'root'
 default['provisioner']['group'] = 'root'
 
 # Knife options
-default['provisioner']['knife']['ssl_verify'] = ''
+default['provisioner']['knife']['ssl_verify'] = 'verify_none'
 
 # Driver Configuration
 default['provisioner']['driver'] = \
@@ -25,11 +28,11 @@ default['provisioner']['driver'] = \
 }
 default['provisioner']['driver']['machine_options'] = \
 {
-  'ssh_username' => 'ubuntu',
+  'ssh_username' => 'root',
   'use_private_ip_for_ssh' => false,
   'bootstrap_options' => {
     'key_name' => 'dev-provisioner',
-    'image_id' => 'ami-b99ed989',
-    'instance_type' => 'm3.medium'
+    'image_id' => 'ami-c2a818aa',
+    'instance_type' => 't2.small'
   }
 }
